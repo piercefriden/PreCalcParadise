f8a0faea482de9d5d23f798e7323360eaf9a700e
piercefriden <piercefri@outlook.com>
2024-09-10 13:34:05 -0700

    boom

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
- $("#hide").click(function(){
-    $("title1").hide();
-  });
+	$("#hide").click(function(){
+		$(".title1").hide();
+		$(".palm").hide();
+		$(".btn").hide();
+		$(".quiz1").html("question 1");
+		$(".question").html("what transformation occured from the parent function in this equation? y=(x+2)^2");
+		$(".btn1").html("left twice");
+		$(".btn2").html("up twice");
+		$(".btn3").html("right twice");
+	});
+	$("#answerW").click(function(){
+		$(".quiz1").hide("question 1");
+	}};
+	$("#answerW2").click(function(){
+		$(".quiz1").hide("question 1");
+	}};
+	$("#answerR").click(function(){
+		$(".quiz1").hide("question 1");
+	}};
  });
