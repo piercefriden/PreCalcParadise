37c41ae8e2f5a77a818e2690147eed50e4349376
piercefriden <piercefri@outlook.com>
2024-09-11 13:58:40 -0700

    msm

--- script.js
+++ script.js
@@ -5,17 +5,35 @@ $(document).ready(function(){
 		$(".btn").hide();
 		$(".quiz1").html("question 1");
 		$(".question").html("what transformation occured from the parent function in this equation? y=(x+2)^2");
-		$(".btn1").html("left twice");
-		$(".btn2").html("up twice");
-		$(".btn3").html("right twice");
+		$(".btn1").show();
+		$(".btn2").show();
+		$(".btn3").show();
 	});
 	$("#answerW").click(function(){
 		$(".quiz1").hide();
+		$(".question").hide();
+		$(".btn1").hide();
+		$(".btn2").hide();
+		$(".btn3").hide();
+		$(".wrong").html("You're wrong. apply yourself.");
+		$(".btnwrong").show();
 	});
 	$("#answerW2").click(function(){
 		$(".quiz1").hide();
+		$(".question").hide();
+		$(".btn1").hide();
+		$(".btn2").hide();
+		$(".btn3").hide();
+		$(".wrong").html("You're wrong. apply yourself.");
+		$(".btnwrong").show();
 	});
 	$("#answerR").click(function(){
 		$(".quiz1").hide();
+		$(".question").hide();
+		$(".btn1").hide();
+		$(".btn2").hide();
+		$(".btn3").hide();
+		$(".right").html("You're Right!");
+		$(".btnwrong").show();
 	});
  });
